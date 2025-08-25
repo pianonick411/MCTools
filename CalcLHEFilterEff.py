fed1c892893eee7fdfa3e69d1ae3602073e7bb00
Nicholas <npinto7@jh.edu>
2025-08-25 16:34:07 +0200

    More changes.

--- CalcLHEFilterEff.py
+++ CalcLHEFilterEff.py
@@ -10,19 +10,32 @@ import tarfile
 import pathlib
 from tqdm import tqdm
 
-def openGridpack(gridPackFile, odir, chunkNum): 
+def openGridpack(gridPackFile, odir, chunkNum, subFrom): 
     if not os.path.exists(odir): 
         print(odir, "not found! Making ", odir)
         os.makedirs(odir, mode=0o777)
+        os.chmod(odir, 0o777)
     gPackName = gridPackFile.split("/")[-1].rstrip(".tgz")
     gpath = odir+"/"+gPackName+"_"
     # We have to make a directory for each job, because we don't want to have the jobs overwriting each others' output. 
     # In theory, you could modify runcmsgrid.sh to take a command line argument with a new name for each job and only make one directory, but the structure of runcmsgrid.sh isn't consistent across gridpacks, so this would be exceptionally difficult. 
     if not os.path.exists(gpath+str(chunkNum)): 
         os.makedirs(gpath+str(chunkNum), mode=0o777)
+        os.chmod(gpath+str(chunkNum), 0o777)
     with tarfile.open(gridPackFile) as tar: 
         tar.extractall(path=gpath+str(chunkNum), filter="fully_trusted")
-    
+    if not os.path.exists(subFrom): 
+        print(subFrom, " not found! Making ", subFrom)
+        os.makedirs(subFrom, mode=0o777)
+
+    with open(f"{subFrom}/submit_Chunk_{chunkNum}.sh", "w") as f:
+        output = f"""#!/bin/bash
+cd {gpath}{chunkNum}
+./runcmsgrid.sh $1 $2 $3"""
+        f.writelines(output) 
+    os.chmod(f"{subFrom}/submit_Chunk_{chunkNum}.sh", 0o777)
+
+
 
 
 
@@ -42,6 +55,7 @@ def main(raw_args=None):
     outputdir = os.path.abspath(args.odir)
     NEvents = args.NEvents
     chunkSize = args.Size
+    
 
     n_jobs_raw, remainder = divmod(NEvents, chunkSize)
     if remainder == 0: 
@@ -56,6 +70,8 @@ def main(raw_args=None):
 
     gPackName = inputGridpack.split("/")[-1].rstrip(".tgz")
     gPath = outputdir+"/"+gPackName+"_"
+
+    subFrom = args.subfrom + f"submit_{gPackName}"
     
     itemdata = []
     for i in tqdm(range(0, n_jobs), desc="Gridpack dirs opened"):
@@ -68,7 +84,7 @@ def main(raw_args=None):
         else: 
             itemdata.append({"chunk_arguments": f"{chunkSize} {seedList[i]} 1"})
         
-        openGridpack(inputGridpack, outputdir, i)
+        openGridpack(inputGridpack, outputdir, i, subFrom)
 
          
     
@@ -76,21 +92,20 @@ def main(raw_args=None):
 
     jobs = htcondor.Submit(
         {
-            # "executable": f"{gPath}$(ProcId)/runcmsgrid.sh", 
-            "executable": f"/bin/cat",
-            # "arguments": "$(chunk_arguments)", 
-            "arguments": "/afs/cern.ch/user/n/nipinto/private/MCTools/README.md",
-            "output": f"{outputdir}/job_$(ProcId).out",
-            "error": f"{outputdir}/job_$(ProcId).err",
-            "log": f"{outputdir}/log.log",
+            "executable": f"{subFrom}/submit_Chunk_$(ProcId).sh", 
+            "arguments": "$(chunk_arguments)", 
+            "output": f"{subFrom}/job_$(ProcId).out",
+            "error": f"{subFrom}/job_$(ProcId).err",
+            "log": f"{subFrom}/log.log",
             "request_memory": "4000M",
             "+JobFlavour": "nextweek",
             "periodic_remove": "JobStatus == 5",
             "WhenToTransferOutput": "ON_EXIT_OR_EVICT",
+
         }
     )    
 
-    # print(jobs)
+    print(jobs)
     
     schedd = htcondor.Schedd()
     submit_result = schedd.submit(jobs, itemdata = iter(itemdata))  # submit one job for each item in the itemdata
